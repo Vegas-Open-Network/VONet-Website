2c4c32c442956b687b95840d5dd25cf0f7ce2146
Jacob Kukuk <me@jacobkukuk.com>
2025-08-31 16:43:49 -0700

    adjusted for donation system

---
+++ Pages/Donate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VONetWebsite.Pages
+{
+    public class DonateModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Page initialization logic can go here if needed
+        }
+    }
+}
